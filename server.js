0a3b48bd2bdd4d17d396d78e0b74ab1dd31ae237
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-06-17 22:56:20 +0600

    Create Better-Auth User Schema

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
---
+++ src/lib/auth.js
@@ -0,0 +1,48 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+export const createAuth = (db) => {
+    return betterAuth({
+        database: mongodbAdapter(db),
+
+        baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5000",
+
+        session: {
+            expiresIn: "10d",
+
+            fields: {
+                user: [
+                    "role",
+                    "status",
+                    "phone",
+                    "address",
+                    "bio",
+                    "lastActionDate",
+                ],
+            },
+        },
+
+        user: {
+            additionalFields: {
+                role: {
+                    type: "string",
+                },
+                status: {
+                    type: "string", // "active", "suspended", "blocked"
+                },
+                phone: {
+                    type: "string",
+                },
+                address: {
+                    type: "string"
+                },
+                bio: {
+                    type: "string",
+                },
+                lastActionDate: {
+                    type: "date",
+                }
+            },
+        },
+    });
+};
