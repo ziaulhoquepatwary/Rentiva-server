cc49d93b9665360b8d6f0a3b3d9820684bd9eea1
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-06-18 00:32:51 +0600

    Create verifyToken middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,42 @@
+import { fromNodeHeaders } from "better-auth/node";
+
+export const verifyToken = async (req, res, next) => {
+    try {
+        const auth = req.app.get("auth");
+
+        if (!auth) {
+            throw new Error("Better Auth instance not found in app configuration.");
+        }
+
+        const session = await auth.api.getSession({
+            headers: fromNodeHeaders(req.headers),
+        });
+
+        if (!session || !session.user) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized! Please login first."
+            });
+        };
+
+        // console.log(session);
+
+        req.user = {
+            id: session.user.id,
+            name: session.user.name,
+            email: session.user.email,
+            role: session.user.role,
+            status: session.user.status,
+            lastActionDate: session.user.lastActionDate,
+        };
+
+        next();
+
+    } catch (error) {
+        console.error("Auth Middleware Error:", error);
+        return res.status(401).json({
+            success: false,
+            message: "Session invalid or expired."
+        });
+    }
+}
