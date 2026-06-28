c6af7d1de0d991ebe9f325d7f085220f1e27ecfb
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-06-28 18:34:57 +0600

    Create getTenantFavorites api controller

--- src/modules/favorites/favorite.controller.js
+++ src/modules/favorites/favorite.controller.js
@@ -1,4 +1,5 @@
 import catchAsync from "../../utils/catchAsync.js";
+import Property from "../properties/property.model.js";
 import Favorite from "./favorite.model.js";
 
 
@@ -38,5 +39,36 @@ export const checkFavoriteStatus = catchAsync(async (req, res) => {
     res.status(200).json({
         success: true,
         isSaved: !!existingFavorite,
+    });
+});
+
+export const getTenantFavorites = catchAsync(async (req, res) => {
+    const userId = req.user.id;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const favoriteRecords = await Favorite.find({ userId });
+
+    const propertyIds = favoriteRecords.map(fav => fav.propertyId);
+
+    const total = await Property.countDocuments({ _id: { $in: propertyIds } });
+
+    const favoriteProperties = await Property.find({ _id: { $in: propertyIds } })
+        .select("title location propertyType rent images rentType")
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit);
+
+    res.status(200).json({
+        success: true,
+        message: "Tenant favorite properties fetched successfully",
+        meta: {
+            page,
+            limit,
+            total,
+            totalPage: Math.ceil(total / limit),
+        },
+        data: favoriteProperties,
     });
 });
--- src/modules/favorites/favorite.routes.js
+++ src/modules/favorites/favorite.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { checkFavoriteStatus, toggleFavorite } from "./favorite.controller.js";
+import { checkFavoriteStatus, getTenantFavorites, toggleFavorite } from "./favorite.controller.js";
 import { verifyToken } from "../../middleware/authMiddleware.js";
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post("/toggle", verifyToken, toggleFavorite);
 router.get("/check/:id", verifyToken, checkFavoriteStatus);
+router.get("/my-favorites", verifyToken, getTenantFavorites);
 
 export default router;
